f4abe19805f40359acb739f19255bbdf1c6b6c64
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-12-01 10:07:59 +0000

    added directional comments to timestamp.route

--- server/components/timestamp/timestamp.route.js
+++ server/components/timestamp/timestamp.route.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const timestamp = require('./timestamp');
 const router = express.Router();
-
+/**
+  * This function will get the Timestamp from the url and perform all processing to return the correct response.
+  *
+  */
+//todo: break anonymous function into named function
 router.get('/:stamp', function(req, res, next){
     const params = req.params;
+    //todo: rename for clarity
     var object = {};
     object.title = 'timestamp';
+    //todo: break success/error into seperate functions.
     timestamp.getTimeObject(params.stamp, function(data){
         'use strict';
         console.log("success!!", data);
-
         object.content = data;
     }, function(err, obj){
         'use strict';
