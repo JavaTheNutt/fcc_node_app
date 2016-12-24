9c6f1a831292a0a21495bf0bce746b6c9d5551fe
Joe Wemyss <joewemyss3@gmail.com>
2016-12-24 00:06:32 +0000

    added functionality to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,55 @@
 const gulp  = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const cssMin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const del = require('del');
 const pump = require('pump');
 
 const config = require('./server/app.config');
 const log = require('./server/util/logger');
 
+gulp.task('build', ['minifyVendorScripts','minifyVendorCss','fonts', 'minifyCustomJs', 'minifyCustomCss'], ()=>{
+    'use strict';
+    log.info('app built');
+});
+gulp.task('fonts', () => {
+    'use strict';
+    log.info('moving fonts');
+    return gulp.src(config.getFonts())
+        .pipe(gulp.dest('server/dist'))
+});
 gulp.task('scripts', ['vendorScripts', 'minifyCustomJs'], ()=>{
     'use strict';
     log.info('scripts task running');
 });
-
-gulp.task('vendorScripts', () =>{
+gulp.task('minifyVendorScripts', ['concatVendorScripts'], () =>{
+    'use strict';
+    return gulp.src('server/.tmp/vendor.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('server/dist'))
+});
+gulp.task('concatVendorScripts', () =>{
     'use strict';
     log.info('copy task running');
     return gulp.src(config.getJs())
         .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('server/dist'));
+        .pipe(gulp.dest('server/.tmp'));
+});
+gulp.task('concatVendorCss', () => {
+    'use strict';
+    return gulp.src(config.getCss())
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('server/.tmp'))
+});
+gulp.task('minifyVendorCss', ['concatVendorCss'], () => {
+    'use strict';
+    return gulp.src('server/.tmp/vendor.css')
+        .pipe(cssMin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('server/dist'))
+
 });
 gulp.task('concatCustomJs', () =>{
     'use strict';
@@ -30,9 +63,37 @@ gulp.task('minifyCustomJs',['concatCustomJs'], (cb) =>{
     pump([
         gulp.src('server/.tmp/app.js'),
         uglify(),
+        rename({suffix: '.min'}),
         gulp.dest('server/dist')
          ],
     cb)
 });
-
+gulp.task('minifyCustomCss', ['concatCustomCss'], ()=>{
+    'use strict';
+    log.info('minfy custom css task running');
+    return gulp.src('server/.tmp/app.css')
+        .pipe(cssMin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('server/dist'))
+});
+gulp.task('concatCustomCss', ()=>{
+    'use strict';
+    log.info('custom css task running');
+    return gulp.src('server/public/stylesheets/*.css')
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('server/.tmp'))
+});
+gulp.task('clean', () => {
+    'use strict';
+    log.info('running clean task');
+    clean('server/.tmp');
+    clean('server/dist');
+});
+function clean(path){
+    'use strict';
+    log.info('running delete task for: %s', path);
+    del(path).then(paths => {
+        log.info('deleted files and folders:\n', paths.join('\n'));
+    })
+}
 
--- server/app.config.js
+++ server/app.config.js
@@ -8,18 +8,20 @@ const rootPath = path.join(__dirname, 'public/vendor');
 
 function getVendorJs(){
     'use strict';
+    log.info('retrieving vendor js paths');
     const jsVendorList = [
         path.join(rootPath, 'jquery/dist/jquery.min.js'),
         path.join(rootPath, 'jquery-ui/jquery-ui.min.js'),
         path.join(rootPath, 'bootstrap/dist/js/bootstrap.min.js')
     ];
     jsVendorList.forEach((elem) =>{
-        log.info('path: %s', elem);
+        log.info('path to js file: %s', elem);
     });
     return jsVendorList;
 }
 function getVendorCss(){
     'use strict';
+    log.info('retrieving vendor css')
     const cssVendorList = [
         path.join(rootPath, 'normalize-css/normalize.css'),
         path.join(rootPath, 'bootstrap/dist/css/bootstrap.min.css'),
@@ -27,12 +29,24 @@ function getVendorCss(){
         path.join(rootPath, 'font-awesome/css/font-awesome.css')
     ];
     cssVendorList.forEach((elem) =>{
-        log.info('path: %s', elem);
+        log.info('path to css file: %s', elem);
     });
     return cssVendorList;
 }
-
+function getFonts(){
+    'use strict';
+    log.info('retrieving vendor fonts');
+    const fonts = [
+        path.join(rootPath, 'bootstrap/fonts'),
+        path.join(rootPath, 'font-awesome/fonts')
+    ];
+    fonts.forEach((elem) =>{
+        log.info('path to font directory: %s', elem)
+    });
+    return fonts;
+}
 module.exports = {
     getJs: getVendorJs,
-    getCss: getVendorCss
+    getCss: getVendorCss,
+    getFonts: getFonts
 };
